910be5553a44ab00e4f70ab815b4ef5d3322eae8
Florian Romann thinkpad_fedora <s8flroma@stud.uni-saarland.de>
2022-10-01 19:42:57 +0200

    added default age

--- src/app/risk-calculator/risk-calculator.component.ts
+++ src/app/risk-calculator/risk-calculator.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter} from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import { Therapy, OptionField, DataField } from "./therapy.classes";
 import { THERAPIES_DATA } from "./therapies.data";
 import {skip} from "rxjs";
@@ -8,7 +8,7 @@ import {skip} from "rxjs";
   templateUrl: './risk-calculator.component.html',
   styleUrls: ['./risk-calculator.component.css']
 })
-export class RiskCalculatorComponent {
+export class RiskCalculatorComponent implements OnInit{
   title: string = "Risk Calculator";
   subtitle: string = "Calculate your risk of xy in drölf years.";
 
@@ -17,6 +17,14 @@ export class RiskCalculatorComponent {
 
 
   constructor() {
+  }
+
+  ngOnInit() {
+    for (let therapy of this.therapies) {
+      // should not be the case, but if another value is checked, this does not uncheck the other options,
+      // possibly in resulting into multiple checked options(radio-buttons)
+      therapy.option_fields.map((of) => of.options[0].check())
+    }
   }
 
   calculateRisk(): number {
